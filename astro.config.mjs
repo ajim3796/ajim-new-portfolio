ad243258c5ea5cc63f21d5e96917a621f5bd8515
ajim <n1a1o2k0i@gmail.com>
2025-11-06 09:16:10 +0900

    Github pagesからNetlifyに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx()],
-    site: 'https://ajim3796.github.io',
-    base: '/ajim-new-portfolio',
 });
